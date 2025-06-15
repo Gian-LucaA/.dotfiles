98c6716ee996707566c5f18979de424541f61ea8
Gian-LucaA <gianlucaafting@gmail.com>
2025-06-15 16:54:27 +0200

    Added auto completions to NVIM

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -12,11 +12,11 @@ return {
     opts = {
       auto_install = true,
     },
-    config = function ()
-      require('mason-lspconfig').setup({
-        ensure_installed = { "lua_ls" }
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" },
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
@@ -24,16 +24,19 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
 
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
       lspconfig.lua_ls.setup({})
-      lspconfig.ts_ls.setup({})
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
       vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
     end,
   },
 }
-
